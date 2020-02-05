65289bca91fb0520cb0ae48b954b44deaa70fd82
czartek7 <cezary.cieslinski96@gmail.com>
2020-02-05 14:46:35 +0100

    Update 2.0

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 
 class Main {
 
     public static void main(String[] args) {
 
         BinaryTree B = new BinaryTree();
-        BinaryTree MyBinaryTree = B.createBinaryTree(43);
+
+        Scanner print = new Scanner(System.in);
+        System.out.println("Welcome to your Binary Tree Creator!");
+        System.out.println();
+        System.out.print("How many nodes would you like to include: ");
+        int n = print.nextInt();
+
+        BinaryTree MyBinaryTree = B.createBinaryTree(n);
+        System.out.println();
 
         System.out.println("Unbalanced: ");
         MyBinaryTree.printBinaryTree(MyBinaryTree.root, 0);
@@ -16,17 +24,25 @@ class Main {
         System.out.println("Balanced: ");
         TreePrinter.print(MyBinaryTree.root);
 
-        Object NodeFind = MyBinaryTree.FindNode(MyBinaryTree.root, 8);
-        System.out.println("Path to the requested value (0 means that value is not present): " + NodeFind);
+        System.out.println();
+        System.out.print("What value would you like to find in your tree: ");
+        int m = print.nextInt();
+        Object NodeFind = MyBinaryTree.FindNode(MyBinaryTree.root, m);
+        System.out.println();
+        System.out.println("Path to the value " + m + " (0 means that value is not present): " + NodeFind);
 
         Object SB = MyBinaryTree.SmallestBiggest(MyBinaryTree.root);
+        System.out.println();
         System.out.println("Biggest and smallest value: " + SB);
 
         int Depth2 = MyBinaryTree.maxDepth(MyBinaryTree.root);
+        System.out.println();
         System.out.println("Depth of the tree before and after balancing: " + Depth1 + " ---> " + Depth2);
-        
+
+        System.out.println();
         System.out.println("Pre-order search: ");
         MyBinaryTree.Pre_order(MyBinaryTree.root);
+        System.out.println();
 
     }
 }
